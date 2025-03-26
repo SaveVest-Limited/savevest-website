87528701fe95976d90eb2af156637f76f2448884
Olabode-cmd <olabodebalogun80@gmail.com>
2025-03-26 16:33:53 +0100

    terms done

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,101 @@
+const Terms = () => {
+    return (
+        <div>
+            <div className="max-w-7xl mx-auto px-4 py-6">
+                <section className="py-24">
+                    <div className="text-center">
+                        <div>
+                            <p className="text-xs uppercase text-blue-500 bg-blue-50 px-3 py-2 rounded-2xl font-semibold inline-block">our terms</p>
+                        </div>
+
+                        <h2 className="text-3xl md:text-4xl lg:text-5xl font-semibold mt-4">Terms And Conditions for Savevest Limited.</h2>
+                    </div>
+                </section>
+
+                <section className="max-w-5xl mx-auto px-8 py-10 rounded-md bg-gray-50 border border-gray-200">
+                    <p className="text-gray-700 text-md">Last Updated: 1st January 2025</p>
+                    <p className="text-gray-700 text-md mt-6">These Terms and Conditions (“Terms”) govern your use of the Savevest Limited online savings and investment platform and its fully functional mobile application (collectively referred to as the “Platform”). By accessing or using the Platform, you agree to be bound by these Terms. If you do not agree with any part of these Terms, please do not use the Platform.</p>
+
+                    <div className="space-y-6 mt-6">
+                        <div>
+                            <h3 className="text-xl md:text-2xl font-semibold mb-3">1. Account Registration</h3>
+                            <p className="text-gray-700 text-sm md:text-base mb-4"><span className="font-semibold">1.1 Eligibility:</span> To use the Platform, you must be at least 18 years old and have the legal capacity to enter into a binding agreement. By using the Platform, you represent and warrant that you meet these eligibility requirements.</p>
+                            <p className="text-gray-700 text-sm md:text-base mb-4"><span className="font-semibold">1.2 Account Creation:</span> To access certain features of the Platform, you must create an account. You agree to provide accurate, current, and complete information during the registration process. You are responsible for maintaining the confidentiality of your account credentials and for all activities that occur under your account.</p>
+                            <p className="text-gray-700 text-sm md:text-base mb-4"><span className="font-semibold">1.3 Account Security:</span> You are responsible for keeping your account information, including your password, secure and for promptly notifying Savevest if you suspect any unauthorized access or use of your account.</p>
+                        </div>
+
+                        <div>
+                            <h3 className="text-xl md:text-2xl font-semibold mb-3">2. Savings and Investment Services</h3>
+                            <p className="text-gray-700 text-sm md:text-base mb-4"><span className="font-semibold">2.1 Platform Description:</span> Savevest provides an online savings and investment platform that allows users to save regularly and earn interest on their savings. The Platform offers various savings plans, investment options, and financial tools to help users achieve their financial goals.</p>
+                            <p className="text-gray-700 text-sm md:text-base mb-4"><span className="font-semibold">2.2 Financial Risks:</span> Savings and investments involve inherent risks, including the risk of loss of principal. Savevest does not guarantee any specific rate of return or investment results. It is your responsibility to carefully evaluate the risks associated with any savings plan or investment option before making any financial decisions.</p>
+                            <p className="text-gray-700 text-sm md:text-base mb-4"><span className="font-semibold">2.3 Financial Advice:</span> Savevest provides information and tools to assist you in managing your savings and investments. However, the information provided on the Platform should not be considered as financial advice. You should consult with a qualified financial advisor before making any investment decisions.</p>
+                        </div>
+
+                        <div>
+                            <h3 className="text-xl md:text-2xl font-semibold mb-3">3. User Responsibilities</h3>
+                            <p className="text-gray-700 text-sm md:text-base mb-4"><span className="font-semibold">3.1 Compliance:</span> You agree to comply with all applicable laws, regulations, and these Terms when using the Platform. You will not use the Platform for any illegal or unauthorized purposes or engage in any activity that may harm Savevest or other users.</p>
+                            <p className="text-gray-700 text-sm md:text-base mb-4"><span className="font-semibold">3.2 Accuracy of Information:</span> You are responsible for providing accurate, current, and complete information when using the Platform. Savevest relies on the information provided by users and is not responsible for any consequences resulting from inaccurate or incomplete information.</p>
+                            <p className="text-gray-700 text-sm md:text-base mb-4"><span className="font-semibold">3.3 Prohibited Conduct:</span> You agree not to engage in any of the following prohibited conduct when using the Platform:</p>
+
+                            <ul className="text-gray-700 mb-4 pl-6">
+                                <li>a) Violating any applicable laws or regulations.</li>
+                                <li>b) Impersonating another person or entity.</li>
+                                <li>c) Interfering with the operation of the Platform or disrupting its servers or networks.</li>
+                                <li>d) Uploading or transmitting any viruses, malware, or other malicious code.</li>
+                                <li>e) Attempting to gain unauthorized access to the Platform or other users&apos; accounts.</li>
+                            </ul>
+                        </div>
+
+                        <div>
+                            <h3 className="text-xl md:text-2xl font-semibold mb-3">4. Intellectual Property</h3>
+                            <p className="text-gray-700 text-sm md:text-base mb-4"><span className="font-semibold">4.1 Ownership:</span> Savevest retains all rights, title, and interest in and to the Platform and its content, including but not limited to text, graphics, logos, trademarks, and software. These Terms do not grant you any rights or licenses to use Savevest&apos;s intellectual property, except as expressly authorized by these Terms.</p>
+                            <p className="text-gray-700 text-sm md:text-base mb-4"><span className="font-semibold">4.2 User Content:</span> Using the Platform, you grant Savevest a non-exclusive, royalty-free, worldwide license to use, reproduce, modify, publish, and distribute any content you submit, post, or display. You represent and warrant that you have the necessary rights to grant this license.</p>
+                        </div>
+
+                        <div>
+                            <h3 className="text-xl md:text-2xl font-semibold mb-3">5. Privacy</h3>
+                            <p className="text-gray-700 text-sm md:text-base mb-4">Savevest is committed to protecting your privacy and handling your personal information by applicable data protection laws. Please review our Privacy Policy for details on how we collect, use, disclose, and protect your personal information.</p>
+                        </div>
+
+                        <div>
+                            <h3 className="text-xl md:text-2xl font-semibold mb-3">6. Limitation of Liability</h3>
+                            <p className="text-gray-700 text-sm md:text-base mb-4"><span className="font-semibold">6.1 Disclaimer:</span> The Platform is provided on an “as-is” and “as-available” basis without any warranties or representations, express or implied. Savevest disclaims all warranties, including but not limited to the platform’s accuracy, reliability, availability, or content.</p>
+                            <p className="text-gray-700 text-sm md:text-base mb-4"><span className="font-semibold">6.2 Limitation of Liability: </span> Savevest and its affiliates, officers, directors, employees, and agents will not be liable for any indirect, incidental, special, consequential, or punitive damages, whether based on warranty, contract, tort, or any other legal theory, arising out of or in connection with your use of the Platform.</p>
+                        </div>
+
+                        <div>
+                            <h3 className="text-xl md:text-2xl font-semibold mb-3">7. Indemnification</h3>
+                            <p className="text-gray-700 text-sm md:text-base mb-4">You agree to indemnify and hold Savevest, its affiliates, officers, directors, employees, and agents harmless from any claims, losses, liabilities, damages, costs, or expenses, including reasonable attorneys’ fees, arising out of or in connection with your use of the Platform, violation of these Terms, or any violation of applicable laws or regulations.</p>
+                        </div>
+
+                        <div>
+                            <h3 className="text-xl md:text-2xl font-semibold mb-3">8. Modifications and Termination</h3>
+                            <p className="text-gray-700 text-sm md:text-base mb-4"><span className="font-semibold">8.1 Modifications:</span> Savevest reserves the right to modify or update these Terms at any time without prior notice. The updated Terms will be effective upon posting on the Platform. Your continued use of the Platform after the posting of any changes constitutes your acceptance of the modified Terms.</p>
+                            <p className="text-gray-700 text-sm md:text-base mb-4"><span className="font-semibold">8.2 Termination: </span> Savevest may terminate or suspend your access to the Platform, in whole or in part, at any time and for any reason, without prior notice or liability. Upon termination, these Terms will also terminate, except for provisions that by their nature should survive.</p>
+                        </div>
+
+                        <div>
+                            <h3 className="text-xl md:text-2xl font-semibold mb-3">9. Governing Law and Dispute Resolution</h3>
+                            <p className="text-gray-700 text-sm md:text-base mb-4">These Terms and any disputes arising out of or in connection with these Terms or the Platform will be governed by and construed under the laws of The Federal Republic of Nigeria. Any legal action or proceeding arising under these Terms shall be resolved exclusively in the courts of The Federal Republic of Nigeria.</p>
+                        </div>
+
+                        <div>
+                            <h3 className="text-xl md:text-2xl font-semibold mb-3">10. Entire Agreement</h3>
+                            <p className="text-gray-700 text-sm md:text-base mb-4">These Terms constitute the entire agreement between you and Savevest regarding your use of the Platform and supersede any prior agreements or understandings, whether oral or written.</p>
+                            <p className="text-gray-700 text-sm md:text-base mb-4">If you have any questions or concerns regarding these Terms, please get in touch with us at:</p>
+
+                            <ul className="text-gray-700 mb-4">
+                                <li className="font-semibold">SaveVest Limited.</li>
+                                <li><span className="font-semibold">Address:</span> 2a, Lateef Jakande Road, Agidingbi, Ikeja, Lagos State.</li>
+                                <li><span className="font-semibold">Email:</span> <a className='text-blue-500 underline' href="mailto:support@savevesting.com">support@savevesting.com</a>, <a className='text-blue-500 underline' href="mailto:savevestsavers@gmail.com">savevestsavers@gmail.com</a></li>
+                                <li><span className="font-semibold">Phone:</span> +234(0)703-149-4931</li>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    )
+}
+
+export default Terms;
